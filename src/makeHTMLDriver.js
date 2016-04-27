e341f113685d24d07526dceca75b523ef89ccf89
Jono Mingard <reason.koan@gmail.com>
2016-04-27 12:02:08 +1200

    Rewrite HTML driver to work with maquette vTrees

--- src/makeHTMLDriver.js
+++ src/makeHTMLDriver.js
@@ -1,6 +1,23 @@
 import Rx from 'rx'
-import toHTML from 'snabbdom-to-html'
 import {transposeVTree} from './transposition'
+
+const VOID_ELEMENTS = [
+  "area",
+  "base",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "keygen",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr"
+]
 
 function makeBogusSelect() {
   return function select() {
@@ -13,9 +30,100 @@ function makeBogusSelect() {
   }
 }
 
+function truthyValuedKeys(object) {
+  return Object.keys(object).filter(key => object[key]);
+}
+
 function makeHTMLDriver() {
+  function tagNameFromSelector(selector) {
+    const match = selector.match(/^[^#\.]+/);
+
+    return match ? match[0] : "div";
+  }
+
+  function idFromSelector(selector) {
+    const match = selector.match(/#[^#\.]+/g);
+
+    return match && match[0];
+  }
+
+  function classListFromSelector(selector) {
+    const match = selector.match(/\.[^#\.]+/g);
+
+    return match ? match.map(part => part.slice(1)) : [];
+  }
+
+  function mergeProperties(id, classList, properties = {}) {
+    let html, attrs = { id };
+
+    for (let propName of Object.keys(properties)) {
+      let propValue = properties[propName];
+
+      if (propName === 'key' || propValue === null || propValue === undefined) {
+        continue;
+      } else if (propName === 'class') {
+        propValue.split(" ").forEach(className => classList.push(className));
+      } else if (propName === 'classes') {
+        // object with string keys and boolean values
+        truthyValuedKeys(propValue).forEach(className => classList.push(className));
+      } else if (propName === 'styles') {
+        // object with string keys and string (!) values
+        attrs.style = Object.keys(propValue).map(name =>
+          `${name}: ${propValue[name]};`
+        );
+      } else {
+        if (typeof(propValue) === 'function') {
+          continue;
+        } else if (propName === 'innerHTML') {
+          html = propValue;
+        } else {
+          attrs[propName] = propValue;
+        }
+      }
+    }
+
+    attrs.class = classList.join(" ");
+
+    return { attrs, html };
+  }
+
+  function vnodeToHTML(vnode) {
+    // console.log(vnode);
+    let selector = vnode.vnodeSelector;
+
+    if (selector === '') {
+      return vnode.text;
+    } else {
+      const tagName = tagNameFromSelector(selector);
+      let { attrs, html } = mergeProperties(
+        idFromSelector(selector),
+        classListFromSelector(selector),
+        vnode.properties
+      );
+
+      if (!html) {
+        html = vnode.children ?
+          vnode.children.map(child => vnodeToHTML(child)).join("") :
+          vnode.text;
+      }
+
+      attrs = Object.keys(attrs).filter(name => attrs[name])
+        .map(attr => `${attr}="${attrs[attr]}"`).join(" ");
+
+      if (attrs.length > 0) {
+        attrs = " " + attrs;
+      }
+
+      if (VOID_ELEMENTS.indexOf(tagName) >= 0) {
+        return `<${tagName}${attrs} />`
+      } else {
+        return `<${tagName}${attrs}>${html}</${tagName}>`
+      }
+    }
+  };
+
   return function htmlDriver(vtree$) {
-    let output$ = vtree$.flatMapLatest(transposeVTree).last().map(toHTML)
+    let output$ = vtree$.flatMapLatest(transposeVTree).last().map(vnodeToHTML)
     output$.select = makeBogusSelect()
     return output$
   }
